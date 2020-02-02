b46df4eaac6974a6cb082da1ee3f76f049079549
ayite86 <ayite.folly@gmail.com>
2020-02-02 23:39:59 +0100

    la fonction createHeros et createPlayers sont pratiquement fini

--- rpg/main.swift
+++ rpg/main.swift
@@ -7,46 +7,43 @@
 //
 
 import Foundation
-func createHeros(name : String)-> [Hero]{
-    var heroTable : [Hero] = []
-    var heroTypeTable : [Hero.Cate] = []
-    let sentenceTable = ["Fais ton premier choix","HA HA !tres bon choix" + "\nFais ton second choix", "Fais ton dernier choix !!!"]
-    
-    for i in 0...2{
-        print("\n\(sentenceTable[i])"
-            + "\n 1. L'archer"
-            + "\n 2. Le guerrier"
-            + "\n 3. L'espion"
-            + "\n 4. Le magicien"
-            + "\n 5. Le MONSTRE")
-        if let firsChoice = readLine() {
-            switch firsChoice {
-            case "1":
-                heroTypeTable.append(.archer)
-            case "2":
-                heroTypeTable.append(.guerrier)
-            case "3":
-                heroTypeTable.append(.espion)
-            case "4":
-                heroTypeTable.append(.magicien)
-            case "5":
-                heroTypeTable.append(.MONSTRE)
-            default:
-                print("Je n'ai pas compris la réponse")
-            }
-            print(heroTypeTable[i])
+func createHeros(name : String, compteur: Int,sentenceTable: [String])-> Hero{
+    let hero: Hero
+    var heroType : Hero.Cate?
+    print("\n\(sentenceTable[compteur])"
+        + "\n 1. L'archer"
+        + "\n 2. Le guerrier"
+        + "\n 3. L'espion"
+        + "\n 4. Le magicien"
+        + "\n 5. Le MONSTRE")
+    if let firsChoice = readLine() {
+        switch firsChoice {
+        case "1":
+            heroType = .archer
+        case "2":
+            heroType = .guerrier
+        case "3":
+            heroType = .espion
+        case "4":
+            heroType = .magicien
+        case "5":
+            heroType = .MONSTRE
+        default:
+            print("Je n'ai pas compris la réponse")
         }
-        let hero = Hero(cate: heroTypeTable[i])
-        heroTable.append(hero)
     }
-    return heroTable
+    hero = Hero(cate: heroType!)
+    return hero
 }
 func creatPlayers()-> [Player]{
     var nameP1 : String?
     var nameP2 : String?
     var player1 : Player
-    var heroTab : [Hero] = []
+    var player2 : Player
+    var heroTab1 : [Hero] = []
+    var heroTab2 : [Hero] = []
     var joueurTab : [Player] = []
+    var joueur2Tab : [Player] = []
     print("\nHA HA Un nouveau guerrier est la !!!"
         + "\nQu'elle est ton nom ?")
     if let namePlayer1 = readLine() {
@@ -60,8 +57,12 @@ func creatPlayers()-> [Player]{
             case "1" :
                 print("Tu choisis de venir seul ! COURAGEUX")
                 print("Tu dois avoir trois guerriers dans ton équipe")
-                heroTab = createHeros(name : nameP1!)
-                player1 = Player(name: nameP1!, numberOfPlayers: 1,warriors1: heroTab[0], warriors2: heroTab[1], warriors3: heroTab[2])
+                for i in 0...2 {
+                    let sentenceTable = ["Fais ton premier choix","HA HA !tres bon choix \(nameP1)" + "\nFais ton second choix", "Fais ton dernier choix !!!"]
+                    let heroPlayer1 = createHeros(name : nameP1!, compteur: i,sentenceTable: sentenceTable)
+                    heroTab1.append(heroPlayer1)
+                }
+                player1 = Player(name: nameP1!, numberOfPlayers: 1,warriors1: heroTab1[0], warriors2: heroTab1[1], warriors3: heroTab1[2])
                 joueurTab.append(player1)
                 print("Félicitation tu possedes tes trois héros."
                     + "\n Commencons la partie")
@@ -70,6 +71,15 @@ func creatPlayers()-> [Player]{
                     + "Joueur 2 presentes toi !")
                 if let namePlayer2 = readLine(){
                     nameP2 = namePlayer2
+                    print("Vous devez avoir chacun trois Héros pour vous foutres dessus")
+                    for i in 0...2 {
+                        let sentencePlayer1Table = ["\(nameP1) Fais ton premier choix","HA HA !tres bon choix \(nameP1)" + "\nFais ton second choix", "\(nameP1) Fais ton dernier choix !!!"]
+                        let heroPlayer1 = createHeros(name : nameP1!, compteur: i,sentenceTable: sentencePlayer1Table)
+                        heroTab1.append(heroPlayer1)
+                        let sentencePlayer2Table = ["\(nameP2) A ton tour, Fais ton premier choix","Bon choix \(nameP2)" + "\nFais ton second choix", "\(nameP2) Fais ton dernier choix !!!"]
+                        let heroPlayer2 = createHeros(name : nameP2!, compteur: i,sentenceTable: sentencePlayer2Table)
+                        heroTab1.append(heroPlayer2)
+                    }
                 }
             default :
                 print("tu begailles ma parole !")
